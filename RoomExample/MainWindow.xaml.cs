9683539e04b8fbd7ef951f768c68a37baca30da5
VikaReshetnik <118896611+VikaReshetnik@users.noreply.github.com>
2023-04-24 10:04:31 +0300

    Add files via upload

---
+++ RoomExample/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RoomLibrary;
+
+namespace RoomExample
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Room room1 = new Room();
+        Room room2 = new Room();
+        Random rnd = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Открытие первой комнаты
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonOpen1_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpen2.IsEnabled = true; //разрешить открыть вторую комнату
+                                          //инициализация полей класса и генерация числа человек
+            room1.RoomLength = rnd.Next(2, 11);
+            room2.RoomWidth = rnd.Next(2, 11);
+            int numP = rnd.Next(1, 9);
+
+            //вывод сгенерированных данных в метки
+            LabelLenght1.Content = room1.RoomLength;
+            LabelWindth1.Content = room1.RoomWidth;
+            LabelNumPerson1.Content = numP;
+
+            //вычисление периметра, площади, метража и вывод в метки
+            LabelPerimetr1.Content = room1.RoomPerimeter();
+            LabelArea1.Content = room1.RoomArea();
+            LabelPersonArea1.Content = room1.PersonArea(numP);
+
+            private void ButtonOpen2_Clicl(object sender, RoutedEventArgs e)
+            {
+                ButtonAll.IsEnable = true; //разрешить суммировать площади комнат
+                room2.RoomLength = Convert.ToDouble(TBLength2.Text);
+                room2.RoomWidth = Convert.ToDouble(TBWidth2.Text);
+                int numP = Convert.ToInt32(TBNumberPerson2.Text);
+
+                //вычисление периметра, площади, метража и вывод в метки
+                LabelPerimeter2.Content = room2.RoomPerimeter();
+                LabelArea2.Content = room2.RoomArea();
+                LabelPersonArea2.Content = Math.Round(room2.PersonArea(numP), 3);
+
+                private void ButtonAll_Click(object sender, RoutedEventArgs e)
+                {
+                    LabelAllArea.Content = room1.RoomArea() + room2.RoomArea();
+                }
+
+
+
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RoomLibrary/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomLibrary
+{
+    public class Room
+    {
+        double roomLength; //длина комнтаы
+        double roomWidth; //ширина комнтаы
+        public double RoomLength
+        {
+            get { return roomLength; }
+            set { roomLength = value; }
+        }
+        public double RoomWidth
+        {
+            get { return roomWidth; }
+            set { roomWidth = value; }
+        }
+        /// <summary>
+        /// Метод вычисляет перимитр комнаты
+        /// </summary>
+        /// <returns></returns>
+        public double RoomPerimeter()
+        {
+            return 2 * (roomLength + roomWidth);
+        }
+        /// <summary>
+        /// Метод вычисляет площадь комнаты
+        /// </summary>
+        /// <returns></returns>
+        public double RoomArea()
+        {
+            return roomLength * roomWidth;
+        }
+        /// Метод вычисляет число квадратных метров
+        /// на одного человека
+        /// </summary>
+        /// <param name="np">Число человек</param>
+        /// <returns>Возвращает число квадратных метров</returns>
+        public double PersonArea(int np)
+        {
+            return RoomArea() / np;
+        }
+    }
+
+}
